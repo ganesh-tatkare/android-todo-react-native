be85e498a91dc79f266dc3a6fcd2f3d3298ff916
Ganesh <gtatkare42@gmail.com>
2022-07-23 21:45:30 +0530

    fixed scroll issue and bg color for dialog

--- components/PriorityDialog.js
+++ components/PriorityDialog.js
@@ -28,57 +28,55 @@ const PriorityDialog = ({
   });
 
   return (
-    <Provider>
-      <View>
-        <Portal>
-          <Dialog visible={priorityVisible} onDismiss={hideDialog}>
-            <Dialog.Title>Choose Priority</Dialog.Title>
-            <Dialog.Content>
-              <View>
-                <View style={styles.DialogContent}>
-                  <RadioButton
-                    color="red"
-                    value="first"
-                    status={checked === "first" ? "checked" : "unchecked"}
-                    onPress={() => setChecked("first")}
-                  />
-                  <Text>High</Text>
-                </View>
-                <View style={styles.DialogContent}>
-                  <RadioButton
-                    color="orange"
-                    value="second"
-                    status={checked === "second" ? "checked" : "unchecked"}
-                    onPress={() => setChecked("second")}
-                  />
-                  <Text>Medium</Text>
-                </View>
-                <View style={styles.DialogContent}>
-                  <RadioButton
-                    color="green"
-                    value="third"
-                    status={checked === "third" ? "checked" : "unchecked"}
-                    onPress={() => setChecked("third")}
-                  />
-                  <Text>Low</Text>
-                </View>
+    <View>
+      <Portal>
+        <Dialog visible={priorityVisible} onDismiss={hideDialog} style={{borderRadius:20, backgroundColor:"#efe8f6"}}>
+          <Dialog.Title>Choose Priority</Dialog.Title>
+          <Dialog.Content>
+            <View>
+              <View style={styles.DialogContent}>
+                <RadioButton
+                  color="red"
+                  value="first"
+                  status={checked === "first" ? "checked" : "unchecked"}
+                  onPress={() => setChecked("first")}
+                />
+                <Text>High</Text>
               </View>
-            </Dialog.Content>
-            <Dialog.Actions>
-              <Button
-                onPress={() => {
-                  setPriority(checked);
-                  hideDialog();
-                  handleSubmit(checked);
-                }}
-              >
-                Submit
-              </Button>
-            </Dialog.Actions>
-          </Dialog>
-        </Portal>
-      </View>
-    </Provider>
+              <View style={styles.DialogContent}>
+                <RadioButton
+                  color="orange"
+                  value="second"
+                  status={checked === "second" ? "checked" : "unchecked"}
+                  onPress={() => setChecked("second")}
+                />
+                <Text>Medium</Text>
+              </View>
+              <View style={styles.DialogContent}>
+                <RadioButton
+                  color="green"
+                  value="third"
+                  status={checked === "third" ? "checked" : "unchecked"}
+                  onPress={() => setChecked("third")}
+                />
+                <Text>Low</Text>
+              </View>
+            </View>
+          </Dialog.Content>
+          <Dialog.Actions>
+            <Button
+              onPress={() => {
+                setPriority(checked);
+                hideDialog();
+                handleSubmit(checked);
+              }}
+            >
+              Submit
+            </Button>
+          </Dialog.Actions>
+        </Dialog>
+      </Portal>
+    </View>
   );
 };
 
